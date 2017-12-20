8bb75388a1c807b41e5b5bc3639d3c35230f9192
mild20091931 <mild20091931@gmail.com>
2017-12-20 13:14:00 +0700

    [change] Change Index&HomeIntro

---
+++ pages/HomeIntro.js
@@ -0,0 +1,24 @@
+// นี่คือหน้า Home นะ
+import React from 'react'
+import Link from 'next/link'
+import styled from 'styled-components'
+
+const Button = styled.button`
+	border-radius: 3px;
+	padding: 0.25em 1em;
+	background: #FFA644;
+	color: white;
+	border: 2px solid  #FFA644;
+`;
+
+
+const Index = () => (
+	<div className ="container-fluid" >      
+    <div className="row">
+	<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<center><img src="../static/image/Logo WIP Camp 10 SVG.svg"  style={{width:'50%',marginTop:'15%'}} className="img-center"/></center>		
+		<center><Link href="/character"><a><Button style={{width:'7%',marginTop:'1%'}}>ถัดไป</Button></a></Link></center>
+    </div>
+	</div></div>
+)
+export default Index
