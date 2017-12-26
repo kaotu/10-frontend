d158484f293a9ac5d5f4ef3f72354cd9a0e1b54f
DESKTOP-13DH8UD\papop <papop.ru@mail.kmutt.ac.th>
2017-12-26 21:35:55 +0700

    [change] navbar หน้า OurCamp ให้เห็น แค่หน้า moblie

--- components/OurCamp/NavOurCamp.js
+++ components/OurCamp/NavOurCamp.js
@@ -1,22 +1,17 @@
 import react from 'react'
 
 const NavOurCamp = () => (
-    <nav className="navbar navbar-light">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#NAVBAR" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="NAVBAR">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Features</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Pricing</a>
-                </li>
-            </ul>            
+        <div class="collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Features</a>
+                <a class="nav-item nav-link" href="#">Pricing</a>
+                <a class="nav-item nav-link disabled" href="#">Disabled</a>
+            </div>
         </div>
     </nav>
 )
