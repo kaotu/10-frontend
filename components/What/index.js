253927d58b4668c5a58d414bbe40f348a5927832
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-11 03:16:03 +0700

    [change] แก้ navbar ใน what

--- components/What/index.js
+++ components/What/index.js
@@ -4,8 +4,14 @@ import styled from 'styled-components'
 import content from './content.json'
 import Card from '../Core/Card'
 import Story from './Story'
+import Navbar  from '../Core/Navbar'
+import Bg from '../Core/BgStory'
+
 
 const index =()=> (
-    <Card img={<Story/>} text={content.text} />
+    <Bg>
+        <Navbar/>
+        <Card img={<Story/>} text={content.text} />
+    </Bg>
 )
 export default index
