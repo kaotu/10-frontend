581631266348b29b05a08aef1d8e49e3e81967d4
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-10 16:34:09 +0700

    [change] component ของแต่ละหน้า

--- pages/page.js
+++ pages/page.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import What from '../components/What/index'
+import Who from '../components/Who/index'
+import ITdetails from '../components/ITdetails/index'
+
 const Page =()=> (
 	<div>
 		<section>
-
+			<What/>
+		</section>	
+		<section>
+			<Who/>
+		</section>	
+		<section>
+			<ITdetails/>
 		</section>	
 	</div>
 )
