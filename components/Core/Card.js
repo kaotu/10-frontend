f4154d1c93f20d72a5a57863d1da7c3abd1f4f6b
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-10 15:11:56 +0700

    [change] Card แก้ grid

--- components/Core/Card.js
+++ components/Core/Card.js
@@ -22,15 +22,16 @@ const HiddenRespone = styled.div`
 
 const HomeOurCamp = (props) => (
     <BgStory id="outer-container">
+        <Menu />
         <div id="page-wrap" className="container">
             <Div className="d-flex flex-column">
                     <Nav/>  
                 <Row className="d-flex p-2 row justify-content-center">
                     <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                    <div className="bg-secondary float-left" style={{ width: '100%' }}>
-                        <article>Lorem Ipsum คืออะไร?</article>
-                        <p>{props.text}</p>
-                    </div>
+                        <div className="bg-secondary float-left" style={{ width: '100%' }}>
+                            <article>Lorem Ipsum คืออะไร?</article>
+                            <p>{props.text}</p>
+                        </div>
                     </div>
                 </Row>
             </Div>
