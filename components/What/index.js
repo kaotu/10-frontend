14664ef3f60611b91f4cf4dca236e4fafcadb227
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-16 21:46:30 +0700

    [change] แก้ Layout What

--- components/What/index.js
+++ components/What/index.js
@@ -11,32 +11,40 @@ const Icon = styled.div`
     margin-top: 5%;
 `
 
-const index =()=> (
-    <Bg className="row">
-        <div className="col-12 col-sm-12 col-md-6"><img src="../../static/image/fullHill.svg"/></div>
-        <div className="col-12 col-sm-12 col-md-6">
-            <h1>WHAT</h1>
-            <span>ค่ายนี้คืออะไร?</span>
-            {content.text}
-            <Icon className="row">
-                <div className="col-6 col-md-3 img-responesive">
-                    <img src="../../static/image/question.svg"/>
+const index =()=> ( 
+    <Bg>
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-sm-12 col-md-6">
+                    <img src="/static/image/fullHill.svg"/>
                 </div>
-                <div className="col-6 col-md-3 img-responesive">
-                    <img src="../../static/image/question.svg"/>
+                <div className="col-12 col-sm-12 col-md-6">
+                    <h1>WHAT</h1>
+                    <span>ค่ายนี้คืออะไร?</span>
+                    <br/>
+                    {content.text}
+                    <Icon className="row">
+                        <div className="col-6 col-md-3 img-responesive">
+                            <img src="/static/image/question.svg"/>
+                        </div>
+                        <div className="col-6 col-md-3 img-responesive">
+                            <img src="/static/image/question.svg"/>
+                        </div>
+                        <div className="col-6 col-md-3 img-responesive">
+                            <img src="/static/image/question.svg"/>
+                        </div>
+                        <div className="col-6 col-md-3 img-responesive">
+                            <img src="/static/image/question.svg"/>
+                        </div>
+                
+                    </Icon>
+                    <div className="row text-center">
+                        <div className="col-6 col-md-3">Java programing</div>
+                        <div className="col-6 col-md-3">IT Functions</div>
+                        <div className="col-6 col-md-3">Network</div>
+                        <div className="col-6 col-md-3">HTML5&CSS</div>
+                    </div>
                 </div>
-                <div className="col-6 col-md-3 img-responesive">
-                    <img src="../../static/image/question.svg"/>
-                </div>
-                <div className="col-6 col-md-3 img-responesive">
-                    <img src="../../static/image/question.svg"/>
-                </div>
-            </Icon>
-            <div className="row text-center">
-                <div className="col-6 col-md-3">Java programing</div>
-                <div className="col-6 col-md-3">IT Functions</div>
-                <div className="col-6 col-md-3">Network</div>
-                <div className="col-6 col-md-3">HTML5&CSS</div>
             </div>
         </div>
     </Bg>
