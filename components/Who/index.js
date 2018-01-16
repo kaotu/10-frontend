445dea3913eb09ba775cbcba6e67fdffe27fde7c
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-16 21:47:39 +0700

    [change] แก้ Layout หน้า Who

--- components/Who/index.js
+++ components/Who/index.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
+
 import content from './content.json'
-import Card from '../Core/Card'
 import Story from './Story'
+import Bg from '../Core/BgStory'
 
 const index =()=> (
-    <Card img={<Story/>} text={content.text} />
+    <Bg>
+        <div className="container">    
+            <div className="row">
+                <div className="row col-12 col-md-6">
+                    <h1>WHAT</h1>
+                    <span>ค่ายนี้คืออะไร?</span>
+                    
+                    {content.text}
+                    <div className="col-6 col-md-3">
+                        <img   src="/static/image/question.svg"/>
+                    </div>
+                    <div className="col-6 col-md-3">
+                        <img src="/static/image/question.svg"/>
+                    </div>
+                    <div className="col-6 col-md-3">
+                        <img src="/static/image/question.svg"/>
+                    </div>
+                    <div className="col-6 col-md-3">
+                        <img src="/static/image/question.svg"/>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6">
+                </div>
+            </div>
+        </div>
+    </Bg>    
 )
 export default index
