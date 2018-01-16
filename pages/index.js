76808cdcf3f1edf1a211151be8c86afd4ec3c56d
mild20091931 <mild20091931@gmail.com>
2018-01-16 22:25:29 +0700

    [change] แก้ navbar มาใส่หน้า what

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import Intro from '../components/Intro/'
-
 import What from '../components/What/index'
 import Who from '../components/Who/index'
 import Where from '../components/Where/index'
-import ITdetails from '../components/ITdetails/index'
 import GameBut from '../components/Core/GameBut'
 import Navbar from '../components/Core/Navbar'
+import FAQs from '../components/FAQs/index'
 
 const index = () => (
 	<div>
-		<Navbar/>                
 		<section>
 			<Intro />
 		</section>
+			<Navbar/>                
 		<section>        	
 			<What />
 		</section>
@@ -24,7 +23,7 @@ const index = () => (
 			<Where/>
 		</section>
 		<section>
-			<ITdetails />
+			<FAQs />
 		</section>
 	</div>
 )
