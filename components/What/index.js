f8dcb9afa731c508a840542258af79cc8e2906b9
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-18 20:07:03 +0700

    [remove] import story หน้า what

--- components/What/index.js
+++ components/What/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+
 import content from './content.json'
 // import Card from '../Core/Card'
-import Story from './Story'
 import Navbar  from '../Core/Navbar'
 import Bg from '../Core/BgStory'
 
