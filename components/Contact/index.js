4df09111e9cb349a2dfc2d5cd2eea5709c7fb0f9
fame <fame@fames-MacBook-Pro.local>
2018-01-19 20:30:48 +0700

    [remove] เอาburger ออก

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -3,55 +3,54 @@ import Bg from '../Core/BgStory.js'
 import styled from 'styled-components'
 
 const DivWhere = styled.div`
-top:200px;
-text-align: center;    
-width: 100%;  
-
+  top:200px;
+  text-align: center;    
+  width: 100%;  
 `
 const Img1 = styled.img`
-width:40%;
-padding:3vw;
+  width:40%;
+  padding:3vw;
 `
 const Img2 = styled.img`
-width:28%;
-padding:1.5vw;
+  width:28%;
+  padding:1.5vw;
 `
 const FrontCon = styled.h1`
-color: #000000;
+  color: #000000;
 `
 
 const index = () => (
-    <DivWhere>
-        <Bg className="d-flex align-items-center">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-12">
-                        <FrontCon>Contact</FrontCon>
-                        <p>ติดต่อเรา</p>
-                        <div class="row">
-                            <div class="col-sm">
-                                <Img1 src="/static/image/ContactPhone.svg"/>
-                                <p>088-8888-888</p>
-                            </div>
-                            <div class="col-sm">
-                                <Img2 src="/static/image/Facebook.svg"/>
-                                <Img2 src="/static/image/Youtube.svg"/>
-                                <Img2 src="/static/image/Ig.svg"/>
-                             </div>
-                            <div class="col-sm">
-                                <Img1 src="/static/image/ContactMail.svg"/>
-                                <p>konlao@gmail.com</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                   <div className="col-md-12">
-                       <p>รอรูปกล่องดวงใจ</p>
-                   </div>
-                </div>
+  <DivWhere>
+    <Bg className="d-flex align-items-center">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <FrontCon>Contact</FrontCon>
+            <p>ติดต่อเรา</p>
+            <div class="row">
+              <div class="col-sm">
+                <Img1 src="/static/image/ContactPhone.svg" />
+                <p>088-8888-888</p>
+              </div>
+              <div class="col-sm">
+                <Img2 src="/static/image/Facebook.svg" />
+                <Img2 src="/static/image/Youtube.svg" />
+                <Img2 src="/static/image/Ig.svg" />
+              </div>
+              <div class="col-sm">
+                <Img1 src="/static/image/ContactMail.svg" />
+                <p>konlao@gmail.com</p>
+              </div>
             </div>
-        </Bg>
-    </DivWhere>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <p>รอรูปกล่องดวงใจ</p>
+          </div>
+        </div>
+      </div>
+    </Bg>
+  </DivWhere>
 )
 export default index
