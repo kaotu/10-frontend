7f5f0615059f6c05f764f4fec97146045168f139
DESKTOP-13DH8UD\papop <papop.ru@mail.kmutt.ac.th>
2018-01-19 17:27:59 +0700

    [create] BurgerMenu

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,33 @@
 import React from 'react'
-import styled from 'styled-components'
+import Intro from '../components/Intro/'
 
-// import Intro from '../components/Intro/'
 import What from '../components/What/index'
 import Who from '../components/Who/index'
 import Where from '../components/Where/index'
 import When from '../components/When/index'
 import GameBut from '../components/Core/GameBut'
 import Navbar from '../components/Core/Navbar'
-import FAQs from '../components/FAQs/index'
-import Contact from '../components/Contact/index'
-
-const Section = styled.section`
-	margin-bottom:5%;
-`
+import Burger from '../components/Core/Burger'
 
 const index = () => (
-	<div>                
-		{/* <section>
-			<Intro />
-		</section> */}
-		<Navbar/>                
-		<Section>        	
+	<div>
+		<Navbar/>
+		<Burger/>                
+		<section>
+			{/* <Intro /> */}
+		</section>
+		<section>        	
 			<What />
-		</Section>
-		<Section>
+		</section>
+		<section>
 			<Who />
-		</Section>
-		<Section>
+		</section>
+		<section>
 			<Where/>
-		</Section>
-		<Section>
+		</section>
+		<section>
 			<When/>
-		</Section>
-		<Section>
-			<FAQs />
-		</Section>
-		<Section>
-			<Contact />
-		</Section>
+		</section>
 	</div>
 )
 
