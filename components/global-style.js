575508ccab3bd59160381eaf2f11adaa8f17cb4a
JewlyTwin <l2jew555@hotmail.com>
2018-01-19 21:00:57 +0700

    [change] front

--- components/global-style.js
+++ components/global-style.js
@@ -1,8 +1,12 @@
 import { injectGlobal } from 'styled-components';
 
 injectGlobal`
-  body{
-     font-family: 'Pridi';
-     src: url('../static/font/Pridi-Light.ttf');
+  @font-face {
+    font-family: 'Pridi';
+    src: url('/static/font/Pridi-Light.ttf');
+  }
+
+  body {
+    font-family: 'Pridi', serif;
   }
 `
