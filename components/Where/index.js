b8cdb0a951d97c33f0ac15969385abd3598e04e7
fame <fame@fames-MacBook-Pro.local>
2018-01-19 16:07:44 +0700

    [change] เพิ่ม target ให้ลิ้งgoogle maps แบบnew tab

--- components/Where/index.js
+++ components/Where/index.js
@@ -34,7 +34,7 @@ const index = () => (
                         <span>ค่ายนี้จัดที่ไหน</span>
                     </Mobile>
                     <div className="text-center">
-                        <a href="https://www.google.co.th/maps/place/%E0%B8%84%E0%B8%93%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%84%E0%B9%82%E0%B8%99%E0%B9%82%E0%B8%A5%E0%B8%A2%E0%B8%B5%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%AA%E0%B8%99%E0%B9%80%E0%B8%97%E0%B8%A8/@13.6525855,100.4914216,17z/data=!3m1!4b1!4m5!3m4!1s0x30e2a251a57396f5:0x2fd829cd26be9e23!8m2!3d13.6525855!4d100.4936103">
+                        <a href="https://www.google.co.th/maps/place/%E0%B8%84%E0%B8%93%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%84%E0%B9%82%E0%B8%99%E0%B9%82%E0%B8%A5%E0%B8%A2%E0%B8%B5%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%AA%E0%B8%99%E0%B9%80%E0%B8%97%E0%B8%A8/@13.6525855,100.4914216,17z/data=!3m1!4b1!4m5!3m4!1s0x30e2a251a57396f5:0x2fd829cd26be9e23!8m2!3d13.6525855!4d100.4936103" target="_blank" >
                             <Local src="static/image/sit-kmutt.jpg"/>
                         </a>
                         <p className="text-muted">คลิกที่รูปภาพเพื่อดูแผนที่</p>
