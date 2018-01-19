62c9be56c11d930ee810b17ea760026a8d5848c1
mild20091931 <mild20091931@gmail.com>
2018-01-19 21:09:02 +0700

    [change] เพิ่ม carousel

---
+++ components/FAQs/ResponFAQs.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import Bg from '../Core/BgStory.js'
+import styled from 'styled-components'
+import Content from './ContentFAQs'
+
+const Img1 = styled.img`
+width:40%;
+padding:3vw;
+`
+const ResponFAQs = () => (
+    
+)
+export default ResponFAQs
