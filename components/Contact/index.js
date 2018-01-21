6c13311c52780e4f9508d60e207f2cc265b22235
mild20091931 <mild20091931@gmail.com>
2018-01-21 14:53:40 +0700

    [change] ใส่ footer + icon ยังไม่ไฟนอล

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,22 +1,47 @@
 import react from 'react'
 import Bg from '../Core/BgStory.js'
 import styled from 'styled-components'
+import HeartBox from './HeartBox'
+import Footer from '../Core/Footer'
 
 const DivWhere = styled.div`
   top:200px;
   text-align: center;    
   width: 100%;  
 `
-const Img1 = styled.img`
+const ImgLeft = styled.img`
   width:40%;
-  padding:3vw;
+  margin-left:8em;
+  padding-top:4em;
+  @media(max-width:720px) {
+    width:10%;
+    margin-left:0em;
+    padding-top:2em;           
+  }
 `
-const Img2 = styled.img`
-  width:28%;
-  padding:1.5vw;
+const ImgCenter = styled.img`
+  width:10%;
+  margin-top:2vw;
+  margin-right:1em;
+  cursor:pointer;
+  padding-top:3em;
+  @media(max-width:720px) {
+    width:10%;
+    margin-right:1em;
+    padding-top:2em;           
+  }
 `
-const FrontCon = styled.h1`
-  color: #000000;
+const ImgRight = styled.img`
+  width:50%;
+  right:0;
+  margin-right:8em;
+  padding-top:1.5em;
+  padding-top:5em;
+  @media(max-width:720px) {
+    width:10%;
+    margin-right:0em;
+    padding-top:4em;           
+  }
 `
 
 const index = () => (
@@ -25,20 +50,22 @@ const index = () => (
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-12">
-            <FrontCon>Contact</FrontCon>
+            <h1>Contact</h1>
             <p>ติดต่อเรา</p>
             <div className="row">
-              <div className="col-sm">
-                <Img1 src="/static/image/ContactPhone.svg" />
+              <div className="col-sm ">
+                <ImgLeft src="/static/image/ContactPhone.svg" />
                 <p>088-8888-888</p>
               </div>
-              <div className="col-sm">
-                <Img2 src="/static/image/Facebook.svg" />
-                <Img2 src="/static/image/Youtube.svg" />
-                <Img2 src="/static/image/Ig.svg" />
+              <div className="col-5-sm ">
+                <ImgCenter src="/static/image/Facebook.svg" />
+                <ImgCenter src="/static/image/Youtube.svg" />
+                <ImgCenter src="/static/image/Ig.svg" />
+                <ImgCenter src="/static/image/Lineicon.svg"/>
+                <ImgCenter src="/static/image/Twitter.svg"/>
               </div>
-              <div className="col-sm">
-                <Img1 src="/static/image/ContactMail.svg" />
+              <div className="col-sm ">
+                <ImgRight src="/static/image/ContactMail.svg" />
                 <p>konlao@gmail.com</p>
               </div>
             </div>
@@ -46,9 +73,14 @@ const index = () => (
         </div>
         <div className="row">
           <div className="col-md-12">
-            <p>รอรูปกล่องดวงใจ</p>
+            <HeartBox/>
           </div>
-        </div>
+          </div>
+          <div className="row">
+          <div className="col-md-12">
+            <Footer/>
+          </div>
+          </div>
       </div>
     </Bg>
   </DivWhere>
--- components/Core/Footer.js
+++ components/Core/Footer.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import RegisBut from '../Core/RegisBut'
-
 const Footer = () => (
-  <div>
-    {/* พื้นหลังของ div นี้เป็นก้อนเมฆ (จะได้ไม่ต้องจัดตำแหน่งซ้อน) */}
     <div className="row">
-      <div className="col-9">
-        {/* รอปุ่มเกม ไว้ริมซ้าย */}
-      </div>
-      <div className="col-3">
-        <RegisBut />
+      <div className="col-12">
+        <p>Footer</p>
       </div>
     </div>
-  </div>
+
 )
 export default Footer
