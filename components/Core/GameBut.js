c60102f36e7924fda36c765246a0fc1762a2809b
JOKNOI <panupong.pj15@mail.kmutt.ac.th>
2018-01-21 17:40:41 +0700

    [change] แก้ปุ่ม GAME

--- components/Core/GameBut.js
+++ components/Core/GameBut.js
@@ -2,44 +2,49 @@ import react from 'react'
 import styled from 'styled-components'
 
 const GameButton = styled.div`
-  width: 180px;
-  height: 60px;    
+  width: 18%;
+  height: 30%;    
   border-radius: 10%;
-  background-color: red;
+  background-color: #E23D37;
   transition:all 550ms ease-in-out;
   cursor: pointer;
   position:fixed;
-  right:-65px;
-  top:200px;          
+  right:-13%;
+  top:35%;          
   z-index: 1;
   color: white;
   letter-spacing: 5px;
-  -webkit-transform:rotate(270deg);
+  padding: 0px;
   @media(max-width:720px) {
-    width: 65px;            
-    height: 65px;            
+    width: 10%;            
+    height: 10%;   
+    right:-3%;         
   }
 `
 
-const Game = styled.span`
-  font-size: 1.5em;
+const Game = styled.div`
+  font-size: 1.4em;
   font-weight: bold;  
-
+  -webkit-transform:rotate(270deg);
+  position: relative;
+  right: 35%;
   @media(max-width:720px) {
-    font-size: 1em;            
+    font-size: 0em;  
+    right: 10%;           
   }
 `
 const IconGame = styled.img`
-  width: 20%;
-  position: relative;
-  buttom: 10px;
-  left: 5%;
+  width: 15%; 
   -webkit-transform:rotate(90deg);
+  @media(max-width:720px) {
+    width: 50%;          
+  }
 `
+
 
 const GameBut = () => (
   <GameButton className="d-flex justify-content-center align-items-center" data-toggle="" data-target="#">
-    <Game className="text-center">GAME</Game><IconGame src="/static/image/icongame-monkey.png"/>
+    <div><Game className="text-center">GAME<IconGame src="/static/image/icongame-monkey.png"/></Game></div>
   </GameButton>
 )
 export default GameBut
