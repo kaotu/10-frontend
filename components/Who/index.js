d2c5e5f4a8737db1c5f18c8ac392259dd23ef8a0
fame <fame@fames-MacBook-Pro.local>
2018-01-22 20:28:15 +0700

    [change] จัดformat

--- components/Who/index.js
+++ components/Who/index.js
@@ -6,7 +6,7 @@ import Bg from '../Core/BgStory'
 const Icon = styled.div`
     margin-top: 5%;
 `
-
+// const 
 const Mobile = styled.div`
     text-align:right;
     @media (max-width:720px) {
@@ -15,34 +15,34 @@ const Mobile = styled.div`
 `
 
 const index =()=> (
-    <Bg who className="d-flex align-items-center">
-        <div className="container">    
-            <div className="row">
-                <div className="col-12 col-sm-12 col-md-6 order-2 order-md-1">
-                    <Mobile>
-                        <h1>WHO</h1>
-                        <span>ค่ายนี้เหมาะกับใคร</span>
-                    </Mobile>
-                    <Icon className="row">
-                        <div className="col-3">
-                            <img src="/static/image/question.svg"/>
-                        </div>
-                        <div className="col-3">
-                            <img src="/static/image/question.svg"/>
-                        </div>
-                        <div className="col-3">
-                            <img src="/static/image/question.svg"/>
-                        </div>
-                        <div className="col-3">
-                            <img src="/static/image/question.svg"/>
-                        </div>
-                    </Icon>
-                </div>
-                <div className="col-12 col-md-6 order-1 order-md-2">
-                    <img src="/static/image/halfHill.svg"/>
-                </div>
+  <Bg who className="d-flex align-items-center">
+    <div className="container">    
+      <div className="row">
+        <div className="col-12 col-sm-12 col-md-6 order-2 order-md-1">
+          <Mobile>
+            <h1>WHO</h1>
+            <span>ค่ายนี้เหมาะกับใคร</span>
+          </Mobile>
+          <Icon className="row">
+            <div className="col-3">
+              <img src="/static/image/question.svg"/>
             </div>
+            <div className="col-3">
+              <img src="/static/image/question.svg"/>
+            </div>
+            <div className="col-3">
+              <img src="/static/image/question.svg"/>
+            </div>
+            <div className="col-3">
+              <img src="/static/image/question.svg"/>
+            </div>
+          </Icon>
         </div>
-    </Bg>    
+        <div className="col-12 col-md-6 order-1 order-md-2">
+          <img src="/static/image/halfHill.svg"/>
+        </div>
+      </div>
+    </div>
+  </Bg>    
 )
 export default index
