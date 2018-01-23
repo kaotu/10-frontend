bd7a3ed32a2273e25e156a7a4118652708169e59
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-23 15:36:43 +0700

    [change] แก้margin heartbox

--- components/Contact/HeartBox.js
+++ components/Contact/HeartBox.js
@@ -3,18 +3,16 @@ import styled from 'styled-components'
 
 const ImgHeart = styled.img`
     width:20vw;
-    margin-top:10em;
     @media(max-width:720px) {
-        margin-top:4em;          
     }
 `
-const BoxHeart = () =>(
-    <div className="container-fluid">
+const BoxHeart = () => (
+  <div className="container-fluid">
     <div className="row">
       <div className="col-md-12 img-responsive">
-        <ImgHeart src="/static/image/BoxHeart.svg"/>
+        <ImgHeart src="/static/image/BoxHeart.svg" />
+      </div>
     </div>
-    </div>
-    </div>
+  </div>
 )
 export default BoxHeart
