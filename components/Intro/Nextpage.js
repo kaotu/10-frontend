8d15e8c6541518be861feab039273849595ab681
JOKNOI <panupong.pj15@mail.kmutt.ac.th>
2018-01-23 20:58:25 +0700

    [change] class to className

--- components/Intro/Nextpage.js
+++ components/Intro/Nextpage.js
@@ -2,6 +2,6 @@ import Link from 'next/link'
 import Button from './Button'
 
 const Nextpage = () => (
-    <center><Link class="animate fadeOutLeft" href="/character"><a><Button style={{width:'7%',marginTop:'1%'}}>ถัดไป</Button></a></Link></center>
+    <center><Link className="animate fadeOutLeft" href="/character"><a><Button style={{width:'7%',marginTop:'1%'}}>ถัดไป</Button></a></Link></center>
 )
 export default Nextpage
