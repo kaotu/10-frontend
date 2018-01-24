6a3a1553cfb77f691a8810456c7b45cf324af9a5
fame <fame@fames-MacBook-Pro.local>
2018-01-24 09:35:24 +0700

    [change] เพิ่ม radius กับ padding

--- components/Who/Block.js
+++ components/Who/Block.js
@@ -4,12 +4,14 @@ const Block = styled.div`
  {
 	position: relative;
 	background: transparent;
-	border: 4px solid #000;
+  border: 4px solid #000;
+  padding: 1em;
+  border-radius: 1em;
 }
   :after, :before {
 	bottom: 100%;
 	left: 50%;
-	border: solid transparent;
+  border: solid transparent;
 	content: " ";
 	height: 0;
 	width: 0;
@@ -21,13 +23,13 @@ const Block = styled.div`
 	border-color: rgba(136, 183, 213, 0);
 	border-bottom-color: #9addde; 
 	border-width: 30px;
-	margin-left: -30px;
+  margin-left: -30px;
 }
  :before {
 	border-color: rgba(194, 225, 245, 0);
 	border-bottom-color: #000;
 	border-width: 36px;
-	margin-left: -36px;
+  margin-left: -36px;
 }
 `
 
