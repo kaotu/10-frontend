9eda2e0f878e50bb7ff5e7bb82c347af2d5ece51
JOKNOI <panupong.pj15@mail.kmutt.ac.th>
2018-01-25 21:33:18 +0700

    [change] ใส่ animate ให้ scroll ลิง ยักษ์

--- components/Intro/Background.js
+++ components/Intro/Background.js
@@ -45,23 +45,47 @@ const PositionGiant = styled.img`
   height: 50vh;
   
 `
+const PositionLing = styled.img`
+  position: absolute;
+  top: 42vh;
+  left: 8vw;
+  width:3vw;
+  animation: MoveUpDown 1s linear infinite;
+  cursor:pointer; 
+
+  @keyframes MoveUpDown {
+    0% {
+      top: 42vh;
+    }
+    50% {
+      top: 40vh;
+    }
+    100% {
+      top: 42vh;
+    }
+  }
+`
+const PositionYak = styled.img`
+  position: absolute;
+  top: 42vh;
+  right: 10vw;
+  width:3vw;
+  animation: MoveUpDown 1s linear infinite;
+  cursor:pointer; 
+
+  @keyframes MoveUpDown {
+    0% {
+      top: 42vh;
+    }
+    50% {
+      top: 40vh;
+    }
+    100% {
+      top: 42vh;
+    }
+  }
+`
 const PositionScrolldown = styled.img`
-position:absolute;
-bottom:15vh;
-width:3vw;
-top:42vh;
-left:8vw;
-cursor:pointer;
-`
-const PositionScrolldown2 = styled.img`
-position:absolute;
-bottom:15vh;
-width:3vw;
-top:42vh;
-right:10vw;
-cursor:pointer;  
-`
-const PositionScrolldown3 = styled.img`
 position:absolute;
 width:3vw;
 top:90vh;
@@ -71,14 +95,14 @@ cursor:pointer;
 
 const Background = () => (
   <div>
-    <PositionScrolldown3 src="/static/image/icon-arrow-white-down.png"/>
-    <PositionScrolldown2 src='/static/image/double-arrow-down-128.png'/>
-    <PositionScrolldown src='/static/image/double-arrow-down-128.png'/>
+    <PositionLing src='/static/image/double-arrow-down-128.png'/>
+    <PositionYak src='/static/image/double-arrow-down-128.png'/>
     {/* <PositionStar src='/static/image/star.svg'/> */}
     {/* <PositionMountain src='/static/image/moutain.svg' /> */}
     {/* <PositionCloud src='/static/image/cloud-bottom.svg' /> */}
     <PositionMonkey src='/static/image/mongkey-Home.svg' />
     <PositionGiant src='/static/image/giantwithcloud.svg' />
+    <PositionScrolldown src="/static/image/icon-arrow-white-down.png"/>
   </div>
 )
 
