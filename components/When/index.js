2a40d9637161bff634b813370a5ed2cc6742ac21
mild20091931 <mild20091931@github.com>
2018-01-25 16:32:10 +0700

    [create] เพิ่ม Box หน้า When

--- components/When/index.js
+++ components/When/index.js
@@ -3,39 +3,57 @@ import Bg from '../Core/BgStory'
 import styled from 'styled-components'
 
 const Img = styled.img`
-  width:40vw;
+    width:40vw;
 `
 
 const Mobile = styled.div`
-  text-align: right;
-  @media (max-width:720px) {
-    text-align:center;
-  }
+    text-align: right;
+    @media (max-width:720px) {
+      text-align:center;
+    }
 `
-
 const H1 = styled.div`
-  font-size: 65px;
-  margin-top:50px;
-  margin-bottom:-25px;
-  margin-left:px;
+    font-size: 65px;
+    margin-top:50px;
+    margin-bottom:-25px;
+    margin-left:px;
 `
 const H2 = styled.div`
-  font-size: 30px;
-  margin-top:0px;
-  margin-bottom:-10px;
+    font-size: 30px;
+    margin-top:0px;
+    margin-bottom:-10px;
 `
+const Box1 = styled.div`
+    width: 20vw;
+    height: 55hw;    
+    padding: 1vw;
+    background-color:#FBFBEF;
+    opecity:0.9;
+    border-radius: 0.2em;
+    margin-top:20vw;
+    margin-left:18vw;   
+`
+
 
 const index = () => (
   <Bg bgColor="#d5eff2" className="d-flex align-items-center">
     <div className="container">
       <div className="row">
-        <div className="col-12 col-sm-12 col-md-6 order-2 text-right">
+        <div className="col-12 col-sm-12 col-md-6 order-2 text-center ">
+          <div className="col-12 col-sm-12 col-md-6 ">
           <Img src="/static/image/Climing.svg" />
+        </div>       
         </div>
-        <div className="col-12 col-sm-12 col-md-6 order-1">
+        <div className="col-12 col-sm-12 col-md-6 order-1 ">
           <Mobile>
             <H1 className="text-center font-weight-bold">WHEN</H1>
             <H2 className="text-center font-weight-bold">ค่ายนี้จัดเมื่อไหร่ ?</H2>
+        <div className="col-12 col-sm-12 col-md-6 ">
+          <Box1 className="text-center">
+            <p>31 มค </p>
+            <p>วันค่าย</p>
+          </Box1>
+        </div>          
           </Mobile>
         </div>
       </div>
