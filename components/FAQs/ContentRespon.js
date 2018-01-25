16b692268917628ea91b2e4044787210c320ddc9
mild20091931 <mild20091931@gmail.com>
2018-01-25 23:09:22 +0700

    [change] แก้ sponsor ไม่ให้ว่าง

--- components/FAQs/ContentRespon.js
+++ components/FAQs/ContentRespon.js
@@ -5,7 +5,6 @@ import Content from '../FAQs/ContentFAQs'
 const ImgLefe = styled.img`
   width:5vw;
   margin-left:8vw;
-
   @media (max-width:720px) {
     width:8vw;
     margin: auto;
@@ -14,7 +13,6 @@ const ImgLefe = styled.img`
 const ImgRight = styled.img`
   width:5vw;
   margin-left:1vw;
-
   @media (max-width:720px) {
     width:8vw;
     margin: auto;
@@ -30,36 +28,35 @@ const Box = styled.div`
   margin-top:1vw;
   margin-bottom:1vw;
   border-radius: 10px; 
-
   @media (max-width:720px) {
     width:42vw;
   }
 `
 
 const ContentRespon = () => (
-    
-    <div className="container-fluid">        
-        <div className="row">
-            <div className="col-3">
-                <div className="img-responesive align-self-center">
-                    <ImgLefe src="/static/image/question.svg" />
-                </div>
-            </div>
-            <div className="col-9">
-                <Box>Q : อยากเป็นส่วนนึงของค่ายนี้มากๆ ต้องทำอย่างไร ?</Box>
-            </div>
+
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-3">
+        <div className="img-responesive align-self-center">
+          <ImgLefe src="/static/image/question.svg" />
         </div>
-        <div className="row">
-            <div className="col-9">
-                <Box>A : น้องๆ เพียงแค่ตอบคำถามในการสมัครด้วยตนเองเพื่อแสดงถึงตัวตนของน้องๆ และมีความตั้งใจในการตอบคำถาม</Box>
-            </div>
-            <div className="col-3">
-                <div className="col img-responesive align-self-center">
-                    <ImgRight src="/static/image/question.svg" />
-                </div>
-            </div>
-        </div>
+      </div>
+      <div className="col-9">
+        <Box>Q : อยากเป็นส่วนนึงของค่ายนี้มากๆ ต้องทำอย่างไร ?</Box>
+      </div>
     </div>
-    
+    <div className="row">
+      <div className="col-9">
+        <Box>A : น้องๆ เพียงแค่ตอบคำถามในการสมัครด้วยตนเองเพื่อแสดงถึงตัวตนของน้องๆ และมีความตั้งใจในการตอบคำถาม</Box>
+      </div>
+      <div className="col-3">
+        <div className="col img-responesive align-self-center">
+          <ImgRight src="/static/image/question.svg" />
+        </div>
+      </div>
+    </div>
+  </div>
+
 )
 export default ContentRespon
--- components/Sponsor/index.js
+++ components/Sponsor/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import H1 from '../Core/FontH1'
+import H2 from '../Core/FontH2'
 
 const Bg = styled.div`
     background-color:#e1f6b3; 
     height:25vw;
 `
 const Index = () => (
-		<Bg/>
+  <Bg>
+    <div className="container-fluid d-flex justify-content-center">
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <H1>Sponsor</H1>
+          <H2 className="font-weight-bold">ยังไม่มีจ้า</H2>
+        </div>
+      </div>
+    </div>
+  </Bg>
 )
 export default Index
