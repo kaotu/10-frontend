211768d9bdf504a01ab9b7da41a6f7b8a03db6e3
JewlyTwin <l2jew555@hotmail.com>
2018-01-25 21:02:04 +0700

    [create] Fontsize.js

--- components/Contact/HeartBox.js
+++ components/Contact/HeartBox.js
@@ -2,7 +2,7 @@ import react from 'react'
 import styled from 'styled-components'
 
 const ImgHeart = styled.img`
-    width:20vw;
+    width:10vw;
     @media(max-width:720px) {
     }
 `
@@ -10,6 +10,7 @@ const BoxHeart = () => (
   <div className="container-fluid">
     <div className="row">
       <div className="col-md-12 img-responsive">
+      <br/><br/><br/><br/><br/><br/><br/>
         <ImgHeart src="/static/image/BoxHeart.svg" />
       </div>
     </div>
