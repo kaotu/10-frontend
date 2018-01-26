179ac2950fd858b0380d03ea47150bd9f6300e50
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-26 23:11:12 +0700

    [change] แก้ fontH1

--- components/Core/FontH1.js
+++ components/Core/FontH1.js
@@ -2,10 +2,7 @@ import react from 'react'
 import styled from 'styled-components'
 
 const H1 = styled.h1`
-    font-size: 5vw;
-    @media (max-height : 720px){
-        font-size: 13vw;
-    }
+    font-size: 5em;
 `
 
 export default H1
