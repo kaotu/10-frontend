db159c95974056b524adb36d26b5a0a01e1f9a99
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-26 23:13:45 +0700

    [change] แก้โครงsponsor

--- components/Sponsor/index.js
+++ components/Sponsor/index.js
@@ -4,16 +4,25 @@ import H1 from '../Core/FontH1'
 import H2 from '../Core/FontH2'
 
 const Bg = styled.div`
-    background-color:#e1f6b3; 
-    height:25vw;
+  background-color:#e1f6b3; 
+  min-height:25vw;
 `
+const Img = styled.img`
+  width: 100%; 
+`
+const Topic = H1.extend`
+  font-size: 3em;
+`
+
 const Index = () => (
-  <Bg>
+  <Bg className="d-flex align-items-center">
     <div className="container-fluid d-flex justify-content-center">
       <div className="row">
         <div className="col-md-12 text-center">
-          <H1>Sponsor</H1>
-          <H2 className="font-weight-bold">ยังไม่มีจ้า</H2>
+          <Topic className="font-weight-bold">Sponsor</Topic>
+          <div className="img-responsive">
+            <Img src="/static/image/sponsor.png"/>
+          </div>
         </div>
       </div>
     </div>
