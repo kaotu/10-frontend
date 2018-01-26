4d4fbce8c79b6bd7a9cacd15365981d4b0d95ae5
fame <fame@fames-MacBook-Pro.local>
2018-01-27 01:00:28 +0700

    [change] เพิ่ม state

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -1,14 +1,11 @@
 // นี่คือหน้า Home นะ
 import React from 'react'
 import styled from 'styled-components'
-import { compose, withState } from 'recompose'
-import Router from 'next/router'
-import ReactTimeOut from 'react-timeout'
+
 import Logo from './Logo'
 import BgIntro from '../BgIntro'
 import LogoSIT from './LogoSIT'
 import Background from './Background'
-import Sponsor from '../Sponsor/index'
 
 
 const Position = styled.div`
@@ -17,20 +14,17 @@ const Position = styled.div`
 	transform: translate(-50%, -50%);
 `
 
-const Index = (props) => (
-  <div>
-  <BgIntro>
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <Background />
-          <Logo />
-        </div>
-      </div>
-    </div>
-  </BgIntro>
-  <Sponsor/>
-  </div>
+const Index = () => (
+	<BgIntro>
+		<div className="container-fluid">
+				<div className="row">
+					<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<Background />
+							<Logo />
+					</div>
+				</div>
+		</div>
+	</BgIntro>
 )
 
 
