e44bde9c15e4fb93c1cac196a1f984ebb0a640f4
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-26 23:58:44 +0700

    [change] แก้โครง sponsor

--- components/Sponsor/index.js
+++ components/Sponsor/index.js
@@ -10,21 +10,58 @@ const Bg = styled.div`
   background-color:#e1f6b3; 
   z-index: 4;
 `
-const Img = styled.img`
-  width: 100%; 
+const BImg = styled.img`
+  width: 30%; 
+  height: 30%;
+  margin-right: 1%;
+  margin-left: 1%;
 `
+const SImg = styled.img`
+  width: 25%; 
+  height: 25%;
+  margin-right: 1%;
+  margin-left: 1%;
+`
+
+const BigSponsor = [
+  { img: "/static/image/sponsor.png" },
+  { img: "/static/image/sponsor.png" },
+  { img: "/static/image/sponsor.png" }
+]
+
+const SmallSponsor = [
+  { img: "/static/image/sponsor.png" },
+  { img: "/static/image/sponsor.png" },
+  { img: "/static/image/sponsor.png" },
+  { img: "/static/image/sponsor.png" },
+  { img: "/static/image/sponsor.png" }
+]
+
 const Topic = H1.extend`
   font-size: 3em;
 `
 
 const Index = () => (
   <Bg className="d-flex align-items-center">
-    <div className="container-fluid d-flex justify-content-center">
+    <div className="container d-flex justify-content-center">
       <div className="row">
         <div className="col-md-12 text-center">
           <Topic className="font-weight-bold">Sponsor</Topic>
           <div className="img-responsive">
-            <Img src="/static/image/sponsor.png"/>
+            <div className="row d-flex justify-content-center">
+              {
+                BigSponsor.map((spon, i) => (
+                  <BImg key={i} src={spon.img} />
+                ))
+              }
+            </div>
+            <div className="row d-flex justify-content-center my-4 mx-1">
+              {
+                SmallSponsor.map((spon, i) => (
+                  <SImg key={i} src={spon.img} />
+                ))
+              }
+            </div>
           </div>
         </div>
       </div>
