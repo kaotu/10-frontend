d2b58dfd477eef3aa694adb5cb732fe242aa3a45
gnbiiie3 <gnbiiie3@hotmail.com>
2018-01-31 01:09:50 +0700

    [change] แก้ระยะขอบหน้า FAQs

--- components/FAQs/ContentRespon.js
+++ components/FAQs/ContentRespon.js
@@ -3,14 +3,14 @@ import styled from 'styled-components'
 import Content from '../FAQs/ContentFAQs'
 
 const ImgLefe = styled.img`
-  width:10vw;
+  width:20vw;
   @media (max-width:720px) {
     width:20vw;
     margin: auto;
   }
 `
 const ImgRight = styled.img`
-  width:5vw;
+  width:10vw;
   @media (max-width:720px) {
     width:20vw;
     right:0;
@@ -28,6 +28,10 @@ const Box = styled.div`
   @media (max-width:720px) {
     width:45vw;
   }
+  ${props=>props.primary && `
+    background-color:#FFFFFF;
+  `}
+
 `
 
 
@@ -45,7 +49,7 @@ const ContentRespon = (props) => (
     </div>
     <div className="row">
       <div className="col-8">
-        <Box><p dangerouslySetInnerHTML={{__html: props.A}}/></Box>
+        <Box primary><p dangerouslySetInnerHTML={{__html: props.A}}/></Box>
       </div>
       <div className="col-4">
           <ImgRight src="/static/image/yakscore-new-01.svg" />
