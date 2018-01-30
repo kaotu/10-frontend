1be06f67f12ee68158c8c03c34920dbafa05634f
JewlyTwin <l2jew555@hotmail.com>
2018-01-31 01:38:48 +0700

    [change] responsive ipad หน้า where

--- components/Where/index.js
+++ components/Where/index.js
@@ -33,10 +33,10 @@ const index = () => (
   <Bg bgColor={Color.mongkey.where} where className="">
     <div className="container-fluid">
       <div className="row">
-        <div className="col-md-6 text-center mt-4">
+        <div className="col-md-12 text-center mt-4">
           <Story src="../../static/image/WhereStory.svg" />
         </div>
-        <div className="col-md-6">
+        <div className="col-md-12">
           <Mobile className="text-center mt-4">
             <H1>WHERE</H1>
             <H2>ค่ายนี้จัดที่ไหน ?</H2>
