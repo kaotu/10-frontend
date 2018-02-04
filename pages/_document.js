e46713b8cfaa94456c5fd903d4e81659134457e1
mild20091931 <mild20091931@gmail.com>
2018-02-05 03:06:11 +0700

    [fix] แก้ mata team

--- pages/_document.js
+++ pages/_document.js
@@ -38,8 +38,8 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/static/image/favicon/favicon-16x16.png"/>
           <link rel="manifest" href="/static/image/favicon/site.webmanifest"/>
           <link rel="mask-icon" href="/static/image/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
-          <meta name="msapplication-TileColor" content="#2b5797"/> 
-          <meta name="theme-color" content="#463b6b"/>
+          <meta name="msapplication-TileColor" content=" #002D40"/> 
+          <meta name="theme-color" content=" #002D40"/>
           {this.props.styleTags}
         </Head>
         <body>
