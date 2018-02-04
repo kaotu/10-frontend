a617eb6617eabb956751a9dac5e432b7986d72c2
fame <fame@fames-MacBook-Pro.local>
2018-02-04 21:11:22 +0700

    [change] แก้ความเร็วเมฆ

--- components/Intro/Background.js
+++ components/Intro/Background.js
@@ -15,7 +15,7 @@ const CloudBack = styled.div`
     
     background: url('/static/image/CloudBG.svg') repeat 0 0;
     background-position: 20vw 0;
-    animation: 100s linear 0s normal none infinite animated;
+    animation: 300s linear 0s normal none infinite animated;
     background-repeat: repeat-x;
     @keyframes animated {
         from {background-position: 20vw 0;}
@@ -37,7 +37,7 @@ const CloudBack1 = styled.div`
     width:100vw;
     
     background: url('/static/image/CloudBG.svg') repeat 0 0;
-    animation: 80s linear 0s normal none infinite animate;
+    animation: 150s linear 0s normal none infinite animate;
     background-repeat: repeat-x;
     @keyframes animate {
         from {background-position:0 0;}
