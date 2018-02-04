6cbe72490952c952a36153dd254055518534a611
JewlyTwin <l2jew555@hotmail.com>
2018-02-04 13:57:01 +0700

    [change] responsive กล่องดวงใจ

--- components/Contact/HeartBox.js
+++ components/Contact/HeartBox.js
@@ -6,13 +6,17 @@ const ImgHeart = styled.img`
     z-index: 2;
     width:20vw;
     left: 39vw;
+    @media(max-width:1500px) {
+      bottom:-3em;
+    }
+    @media(max-width:1025px) {
+      margin-bottom:-5em;
+    }
     @media(max-width:720px) {
-      width: 30%;
-      height: auto;
-      margin-bottom:5vh;
+      bottom:-1em;
     }
 `
 const BoxHeart = () => (
-  <ImgHeart src="/static/image/BoxHeart.svg" className="img-responsive"/>
+  <ImgHeart src="/static/image/BoxHeart.svg"/>
 ) 
 export default BoxHeart
