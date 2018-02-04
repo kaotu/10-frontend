49b39dbe51a09c8cae5841bd6f85b32f0bbb432d
fame <fame@fames-MacBook-Pro.local>
2018-02-05 00:58:53 +0700

    [create] Loading.js

---
+++ components/Core/Loading.js
@@ -0,0 +1,17 @@
+import react from 'react'
+import styled from 'styled-components'
+
+const Bg = styled.section`
+  height:100%;
+  width:100%;
+  background:#002d40;
+  z-index:9999999999999999999;
+  position:absolute;
+`
+const index = () => (
+  <Bg>
+  </Bg>
+
+)
+
+export default index
