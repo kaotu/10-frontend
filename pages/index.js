31762307127d2a26c9af9cba3f8efd1783589ab0
fame <fame@fames-MacBook-Pro.local>
2018-02-04 14:23:28 +0700

    [change] แก้สี footer

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,84 @@
-import React from 'react'
-import Intro from '../components/Intro/'
-import injectGlobal from '../components/global-style'
-import Scroll from '../components/Core/Scroll'
-import What from '../components/What/index'
-import Who from '../components/Who/index'
-import Where from '../components/Where/index'
-import When from '../components/When/index'
-import Faqs from '../components/FAQs'
-import Contact from '../components/Contact'
-import Navbar from '../components/Core/Navbar'
-import Sponsor from '../components/Sponsor/index'
-import styled from 'styled-components'
+import React from "react";
+import Intro from "../components/Intro/";
+import injectGlobal from "../components/global-style";
+import Scroll from "../components/Core/Scroll";
+import What from "../components/What/index";
+import Who from "../components/Who/index";
+import Where from "../components/Where/index";
+import When from "../components/When/index";
+import Faqs from "../components/FAQs";
+import Contact from "../components/Contact";
+import Navbar from "../components/Core/Navbar";
+import Sponsor from "../components/Sponsor/index";
+import styled from "styled-components";
+import { compose, lifecycle, withState } from "recompose";
 
 const Relative = styled.div`
-	position: relative;
-`
+  position: relative;
+`;
 
 const FooterBg = styled.div`
   min-height: 10vh;
-	background-color:#002d40;  
-	border-top: .2em solid #fff;
-`
+  background-color: ${props => props.bgColor || ""};
+  border-top: 0.2em solid #fff;
+`;
 
-injectGlobal
+injectGlobal;
 
-const index = () => (
-	<Scroll>
-		<Relative >		
-		<section id="home" name="home">
-			<Intro />
-		</section>
-		<section id="sponsor">
-			<Sponsor />
-		</section>
-		<Navbar id="navbar-desktop"/>
-		<div data-spy="scroll" data-target="#navbar-desktop" data-offset="0" id="containerElement">
-			<section id="what" name="what">
-				<What />
-			</section>
-			<section id="who" name="who">
-				<Who />
-			</section>
-			<section id="where" name="where">
-				<Where />
-			</section>
-			<section id="when" name="when">
-				<When />
-			</section>
-			<section id="faqs" name="faqs">
-				<Faqs />
-			</section>
-			<section id="contact" name="contact" className="element">
-				<Contact />
-			</section>
-			<FooterBg className="d-flex justify-content-center align-items-center text-white">
-      <div className="fa fa-code" aria-hidden="true" />&nbsp;with&nbsp;<div className="fa fa-heart" aria-hidden="true" />&nbsp;by 10 I'm Developer
-    	</FooterBg>
-		</div>
-		</Relative>
-	</Scroll>
-)
+const index = props => (
+  <Scroll>
+    <Relative>
+      <section id="home" name="home">
+        <Intro />
+      </section>
+      <section id="sponsor">
+        <Sponsor />
+      </section>
+      <Navbar id="navbar-desktop" />
+      <div
+        data-spy="scroll"
+        data-target="#navbar-desktop"
+        data-offset="0"
+        id="containerElement"
+      >
+        <section id="what" name="what">
+          <What />
+        </section>
+        <section id="who" name="who">
+          <Who />
+        </section>
+        <section id="where" name="where">
+          <Where />
+        </section>
+        <section id="when" name="when">
+          <When />
+        </section>
+        <section id="faqs" name="faqs">
+          <Faqs />
+        </section>
+        <section id="contact" name="contact" className="element">
+          <Contact />
+        </section>
+        <FooterBg
+          bgColor={props.color.nav}
+          className="d-flex justify-content-center align-items-center text-white"
+        >
+          <div className="fa fa-code" aria-hidden="true" />&nbsp;with&nbsp;<div
+            className="fa fa-heart"
+            aria-hidden="true"
+          />&nbsp;by 10 I'm Developer
+        </FooterBg>
+      </div>
+    </Relative>
+  </Scroll>
+);
 
-export default index
+export default compose(
+  withState("color", "setColor", ""),
+  lifecycle({
+    componentDidMount() {
+			let theme = JSON.parse(window.localStorage.getItem("color"))
+      this.props.setColor(theme);
+    }
+  })
+)(index);
