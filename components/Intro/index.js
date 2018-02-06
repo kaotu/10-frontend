66e94aeb2cc7328637d781c48d436d3cd6b4bcfa
gnbiiie3 <gnbiiie3@hotmail.com>
2018-02-06 17:09:09 +0700

    [change] ปรับ text: เลื่อนลงข้างล่าง ให้อยู่ตรงกลาง

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -13,42 +13,37 @@ const Div = styled.div`
 `
 
 const Scrolldown = styled.img`
-    position:absolute;
-    z-index: 7;
-    width:3vw;
-    bottom:5vh;
-    left:48vw;
-    animation: Dukdik 1s linear infinite;
-    @keyframes Dukdik {
-      0% {
-        bottom:5vh;
-      }
-      50% {
-        bottom:7vh;
-      }
-      100% {
-        bottom:5vh;
-      }
+  position:absolute;
+  z-index: 7;
+  width:3vw;
+  bottom:5vh;
+  left:48vw;
+  animation: Dukdik 1s linear infinite;
+  @keyframes Dukdik {
+    0% {
+      bottom:5vh;
     }
+    50% {
+      bottom:7vh;
+    }
+    100% {
+      bottom:5vh;
+    }
+}
 `
 const Scroll = styled.h4`
   position:absolute;
   margin-top:40%;
-  margin-left:43.5%;
-  z-index:2561;
+  z-index:10;
   color:#000;
   bottom:0vh;
-  text-align:center;
-  @media (max-width: 720px) {
-    margin-left:38%;
-  }
 `
 const Index = (props) => (
   <div>
       <BgIntro>
         <div className="container-fluid">
           <div className="row">
-            <Div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <Div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center">
               <Background/>
               <Scroll>เลื่อนลงข้างล่าง</Scroll>
 							<Scrolldown src={props.scrolld.scroll}/>
