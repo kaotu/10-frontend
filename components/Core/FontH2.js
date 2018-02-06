e67f2b82d40e9045ded135d3e9b942f84cb62e33
mild20091931 <mild20091931@gmail.com>
2018-02-06 19:39:42 +0700

    [fix] แก้ หัวข้อ ตอน Responsive

--- components/Core/FontH2.js
+++ components/Core/FontH2.js
@@ -5,6 +5,18 @@ const H2 = styled.h2`
     font-size: 2em;
     font-weight: bold;
     color : white;
+    @media (max-width:320px) {
+        font-size: 1.5em;
+    }
+    @media (max-width:420px) {
+        font-size: 1.5em;
+    }
+    @media (max-width:768px) {
+        font-size: 1.5em;
+    }
+    @media (max-width:1024px) {
+        font-size: 1.5em;
+    }
 `
 
 export default H2
