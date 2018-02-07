3498235a313d88c01ed540d1d0cf3ce9a50baae7
gnbiiie3 <gnbiiie3@hotmail.com>
2018-02-07 16:09:13 +0700

    [change] loading

--- components/Core/Loading.js
+++ components/Core/Loading.js
@@ -1,12 +1,15 @@
 import react from 'react'
 import styled from 'styled-components'
+import {compose, withState, lifecycle} from 'recompose'
 
 const Bg = styled.section`
   height:100%;
   width:100%;
-  background:#002d40;
-  z-index:9999999999999999999;
-  position:absolute;
+  background: #002d40;
+  z-index: 99999;
+  position: fixed;
+  display: ${props => props.show ? 'block' : 'none !important'};
+  transition: .4s;
 `
 const Icon = styled.img`
   width:20%;
@@ -25,12 +28,21 @@ const H1 = styled.h1`
   }
 `
 
-const index = () => (
-  <Bg className="d-flex justify-content-center align-items-center">
+const index = ({show}) => (
+  <Bg show={show} className="d-flex justify-content-center align-items-center">
     <Icon src="../../static/image/yakscore-new-01.svg" className="d-flex "/>
     <Icon src="../../static/image/hanumanscore-new-01.svg" className="d-flex "/>
     <H1>กรุณาเลือกตัวละคร</H1>
   </Bg>
 )
 
-export default index
+export default compose(
+  withState('show', 'setShow', true),
+  lifecycle({
+    componentDidMount() {
+      setTimeout(() => {
+        this.props.setShow(false)
+      }, 1000);
+    }
+  })
+)(index)
