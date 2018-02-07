d60e587aa311e2be212cdbccd7068593cbd23a10
JewlyTwin <l2jew555@hotmail.com>
2018-02-07 20:44:24 +0700

    [change] ไม่ใช้รูปหน้าจอเล็กไป

--- pages/minisize.js
+++ pages/minisize.js
@@ -4,15 +4,21 @@ import styled from 'styled-components'
 const Minicon = styled.div`
   display:none;
   width:100%;
+  height: 100vh;
+  background-color: #002D40;
   @media(orientation:landscape)
   and (max-width:1024px){
-    display:block;
+    display:flex;
+    justify-content:center;
+    align-items:center;
   }
 `
 
 const index =()=> (
     <Minicon>
-        <img className="img-fluid" src="../static/image/mini.png"/>
+        {/* <img className="img-fluid" src="../static/image/mini.png"/> */}
+        <p className="text-center">จอเจ้าเล็กไปน้องเอ๋ย เจ้าจงหมุนหน้าจอเป็นแนวตรง ปรับขนาดหน้าต่างเบราเซอร์ <br />
+        หรือใช้ <a href='https://goo.gl/sSUKGW'>เครื่องคณิตกรณ์</a> ที่ขนาดมโหฬารกว่านี้</p>
     </Minicon>
 )
 export default index
