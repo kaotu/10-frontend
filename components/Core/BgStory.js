bc72f99d3d385ddbfff0d53577ac2badacd6f76b
mild20091931 <mild20091931@gmail.com>
2018-02-07 15:38:32 +0700

    [fix] แก้ Bg

--- components/Core/BgStory.js
+++ components/Core/BgStory.js
@@ -4,9 +4,8 @@ const Bg = styled.section`
   background-color: ${props => props.bgColor || ''};
   background-position: center center;
   background-repeat: no-repeat;
-  background-attachment: fixed;
+  background-attachment: auto;
   background-size: cover;
-  min-height: 100vh;
   letter-spacing: 0.01357em;
   position: relative;
   
