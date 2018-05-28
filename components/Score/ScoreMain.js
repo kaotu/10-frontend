6a7744fb254b5f3f1e40be632a903b1236c1dae6
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-05-29 00:46:42 +0700

    [UPDATE] Change Logic by Tae

--- components/Score/ScoreMain.js
+++ components/Score/ScoreMain.js
@@ -1,68 +1,114 @@
 import React from 'react'
 import axios from 'axios'
-import Styled from 'styled-components'
+import Styled, { injectGlobal } from 'styled-components'
 import {api_url} from './api'
 
+injectGlobal`
+  body{
+    background: url(/static/image/scoreboard/bg-2.png) no-repeat center center fixed;
+    background-size: cover;
+    height: 100%;
+    overflow: hidden;
+  }
+`
+const ListBox = Styled.div`
+  background-color:#ccc;
+  padding:1em;
+  border-radius:5px;
+  margin-top:1em;
+  margin-bottom:2em;
+  padding-left:5em;
+`
+const Circle = Styled.div`
+  border-radius:50%;
+  width:65px;
+  height:65px;
+  background-color:#888;
+  color:#fff;
+  position:absolute;
+  left:0;
+  top:12%;
+  padding: 0.75em 1.25em;
+  font-size: 1.4em;
+`
+const Score = Styled.p`
+  right: 8%;
+  top: 19%;
+  margin: 0;
+  font-size: 2em;
+  position:absolute;
+  @media(max-width:576px){
+    right:10%;
+  }
+`
 class ScoreMain extends React.Component {
   
   state = {
     ranking : [],
-    isEqualAll: 0
+    isEqualAll : false
   }
 
   componentDidMount = async () => {
-    let {data : {data}} = await axios({
+    let { data : { data: rawRanking } } = await axios({
       method: 'get',
       url: `${api_url}/scores`
     })
-    data.sort((a,b) => {
+    await rawRanking.sort((a, b) => {
       return a.score < b.score
     })
+    let rankUnFormatted = await this.setRanking(rawRanking)
+    let rankFormatted = await this.formatRanking(rankUnFormatted)
     this.setState({
-      isEqualAll: await this.checkScoreEqualAll(data),
-      ranking: await this.makeRanking(data)
+      ranking: rankFormatted,
+      isEqualAll: this.checkIsEqualAll(rankFormatted)
     })
   }
 
-  checkScoreEqualAll = (data) => {
-    let status = true
-    for (let i = 1; i < data.length; i++) {
-      if(data[0].score !== data[i].score){
-        status = false
-        break
-      }
-    }
-    return status
-  }
-  
-  makeRanking = (data) => {
-    let ranking = [[],[],[],[],[],[],[],[],[],[]]
-    let order = 0
-    for (let i = 0; i < data.length; i++) {
-      if(data[i] != null){
-        ranking[order].push(data[i])
-      }else{
-        continue
-      }
-      for (let j = i+1; j < data.length; j++) {
-        if(data[j] != null && ranking[order][0].score === data[j].score){
-          ranking[order].push(data[j])
-          data[j] = null
+  formatRanking = async (rankUnFormatted) => {
+    return rankUnFormatted.map((rank, i) => {
+      let nextPosition = rankUnFormatted[i+1]
+      if (nextPosition) {
+        if(rank.score === nextPosition.score) {
+          nextPosition.ranking = rank.ranking
+        } else {
+          nextPosition.ranking = rank.ranking + 1
         }
       }
-      order++
-    }
-    return ranking
+      return rank
+    })
+  }
+
+  setRanking = async (ranking) => {
+    return ranking.map((rank) => {
+      return { ...rank , ranking: 1 }
+    })
+  }
+
+  checkIsEqualAll = (rankFormatted) => {
+    return rankFormatted.every((rank) => {
+      return rank.ranking === 1
+    })
   }
 
   render () {
+    let { ranking } = this.state
     return (
       <div className='container'>
-        <div className='row'>
-          <div className='col'>
-            {
-              this.state.isEqualAll === true ? 'true' : 'false'
-            }
+        <div className='row my-4'>
+          <div className='col-12'>
+            <h1 className='text-center'>WIP Camp #10 Ranking</h1>
+          </div>
+          <div className='row justify-content-center'>
+          {
+            this.state.isEqualAll === true ? (
+              this.state.ranking.map((item) => {
+                return <div className='col-7'>
+                          <ListBox><Circle>{item.ranking}</Circle>{item.display_name}<Score>{item.score}</Score></ListBox>
+                       </div>
+              })
+            ):('')
+          }
+
           </div>
         </div>
       </div>
@@ -71,3 +117,5 @@ class ScoreMain extends React.Component {
 }
 
 export default ScoreMain
+
+// this.state.ranking.map((rank, index)=> <div key={index}>{rank.ranking}</div>)
